c59fcf95b42d8ee9e943375a8c4b9e2bd7bf31ed
Santiago Zuluaga Langton <szuluagalangton@hotmail.com>
2024-07-24 20:05:48 -0500

    Changed motion components to use useMotionValue() to improve performance

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,76 +1,101 @@
-import { motion, useScroll, useTransform } from "framer-motion";
-import React, { useRef } from "react";
+import { motion, useScroll, useMotionValue } from "framer-motion";
+import React, { useRef, useEffect } from "react";
 import Voyager from "./Voyager";
 import Position from "./Position";
 
 const Hero = () => {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  })
-  const layer1 = useTransform(scrollYProgress, [0, 1], ["0vh", "200vh"]);
-  const layer2 = useTransform(scrollYProgress, [0, 0.5, 1], ["0vh", "100vh", "150vh"]);
-  const layer3 = useTransform(scrollYProgress, [0, 0.5, 1], ["0vh", "100vh", "100vh"]);
-  const textY = useTransform(scrollYProgress, [0, 0.5, 1], ["0vh", "100vh", "160vh"]);
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start start", "end start"],
+	});
+	const layer1 = useMotionValue("0vh");
+	const layer2 = useMotionValue("0vh");
+	const layer3 = useMotionValue("0vh");
+	const textY = useMotionValue("0vh");
 
-  return (
-    <div
-      ref={ref}
-      className="relative top-0 w-full h-hero overflow-hidden grid"
-    >
-      <motion.div
-        style={{ y: textY }}
-        className="relative z-10 -mt-0"
-      >
-        <div className='parallax__content relative ml-4 top-[8%] sm:ml-8 sm:top-[10%] lg:top-[10%] w-full mx-auto lg:pl-[6rem] lg:pr-[6rem] xl:pl-[10rem] xl:pr-40 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row lg:items-end'>
-          <div className="flex-1 lg:mb-0">
-            <h1 className='font-medium max-w-[80%] text-white text-[40px] sm:text-[38px] md:text-[50px] lg:text-[70px] 2xl:text-[120px] 3xl:text-[150px]'>
-              SANTIAGO ZULUAGA
-            </h1>
-            <Position />
-          </div>
-          <div className="flex-1 flex justify-start lg:justify-center mt-4 sm:mt-14 lg:mt-10 2xl:mt-0">
-            <div className='font-bold text-[25px] mt-8 lg:-mb-4 xl:mb-0 sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left'>
-              Passionate about <br/> science, technology and astronomy.
-            </div>
-          </div>
-        </div>
-      </motion.div>
+	useEffect(() => {
+		const unsubscribeLayer1 = scrollYProgress.onChange((value) =>
+			layer1.set(`${value * 200}vh`)
+		);
 
-      <motion.div
-        className="absolute h-screen inset-0 z-0"
-        style={{
-          backgroundImage: `url(./Layer1.png)`,
-          backgroundPosition: "top",
-          backgroundSize: "cover",
-          y: layer1,
-          willChange: 'transform'
-        }}
-      />
-      <motion.div
-        className="absolute h-screen inset-0 z-20"
-        style={{
-          backgroundImage: `url(./Layer2.png)`,
-          backgroundPosition: "top",
-          backgroundSize: "cover",
-          y: layer2,
-          willChange: 'transform'
-        }}
-      />
-      <motion.div
-        className="absolute h-screen inset-0 z-30"
-        style={{
-          backgroundImage: `url(./Layer3.png)`,
-          backgroundPosition: "top",
-          backgroundSize: "cover",
-          y: layer3,
-          willChange: 'transform'
-        }}
-      />
-      <Voyager />
-    </div>
-  );
-}
+		const unsubscribeLayer2 = scrollYProgress.onChange((value) =>
+			layer2.set(
+				value <= 0.5 ? `${value * 200}vh` : `${(value - 0.5) * 120 + 100}vh`
+			)
+		);
 
-export default React.memo(Hero)
+		const unsubscribeLayer3 = scrollYProgress.onChange((value) =>
+			layer3.set(value <= 0.5 ? `${value * 200}vh` : `${100}vh`)
+		);
+
+		const unsubscribeTextY = scrollYProgress.onChange((value) =>
+			textY.set(
+				value <= 0.5 ? `${value * 200}vh` : `${(value - 0.5) * 150 + 100}vh`
+			)
+		);
+
+		return () => {
+			unsubscribeLayer1();
+			unsubscribeLayer2();
+			unsubscribeLayer3();
+			unsubscribeTextY();
+		};
+	}, [scrollYProgress, layer1, layer2, layer3, textY]);
+
+	return (
+		<div
+			ref={ref}
+			className='relative top-0 w-full h-hero overflow-hidden grid'>
+			<motion.div style={{ y: textY }} className='relative z-10 -mt-0'>
+				<div className='parallax__content relative ml-4 top-[8%] sm:ml-8 sm:top-[10%] lg:top-[10%] w-full mx-auto lg:pl-[6rem] lg:pr-[6rem] xl:pl-[10rem] xl:pr-40 2xl:px-40 3xl:px-60 flex flex-col lg:flex-row lg:items-end'>
+					<div className='flex-1 lg:mb-0'>
+						<h1 className='font-medium max-w-[80%] text-white text-[40px] sm:text-[38px] md:text-[50px] lg:text-[70px] 2xl:text-[120px] 3xl:text-[150px]'>
+							SANTIAGO ZULUAGA
+						</h1>
+						<Position />
+					</div>
+					<div className='flex-1 flex justify-start lg:justify-center mt-4 sm:mt-14 lg:mt-10 2xl:mt-0'>
+						<div className='font-bold text-[25px] mt-8 lg:-mb-4 xl:mb-0 sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left'>
+							Passionate about <br /> science, technology and astronomy.
+						</div>
+					</div>
+				</div>
+			</motion.div>
+
+			<motion.div
+				className='absolute h-screen inset-0 z-0'
+				style={{
+					backgroundImage: `url(./Layer1.png)`,
+					backgroundPosition: "top",
+					backgroundSize: "cover",
+					y: layer1,
+					willChange: "transform",
+				}}
+			/>
+			<motion.div
+				className='absolute h-screen inset-0 z-20'
+				style={{
+					backgroundImage: `url(./Layer2.png)`,
+					backgroundPosition: "top",
+					backgroundSize: "cover",
+					y: layer2,
+					willChange: "transform",
+				}}
+			/>
+			<motion.div
+				className='absolute h-screen inset-0 z-30'
+				style={{
+					backgroundImage: `url(./Layer3.png)`,
+					backgroundPosition: "top",
+					backgroundSize: "cover",
+					y: layer3,
+					willChange: "transform",
+				}}
+			/>
+			<Voyager />
+		</div>
+	);
+};
+
+export default React.memo(Hero);
--- src/components/Voyager.jsx
+++ src/components/Voyager.jsx
@@ -1,88 +1,134 @@
 import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
-import { useScroll, useTransform } from "framer-motion";
+import { useScroll, useTransform, useMotionValue } from "framer-motion";
 import { motion } from "framer-motion-3d";
 
 import voyagerModel from "../assets/3d/voyager.glb";
 import Loader from "./Loader";
 
-const Voyager = ({ scale, position }) => {
-  const { scene } = useGLTF(voyagerModel);
+const Voyager = ({ scale, position, scrollContaienr }) => {
+	const { scene } = useGLTF(voyagerModel);
 
-  const { scrollYProgress } = useScroll();
-  const rotationX1 = useTransform(scrollYProgress, [0, 1], ["0.3", "-1"]);
-  const rotationY1 = useTransform(scrollYProgress, [0, 0.2, 1], ["-0.8", "-1.6", "-10"]);
-  const positionX = useTransform(scrollYProgress, [0, 0.2, 1], [position[0], '0', '-3']);
-  const positionY = useTransform(scrollYProgress, [0, 0.2, 1], [position[1], '-2', '5']);
-  const positionZ = useTransform(scrollYProgress, [0, 0.2, 1], [position[2], '0', '0']);
+	const { scrollYProgress } = useScroll({
+		target: scrollContaienr,
+		offset: ["start start", "end start"],
+	});
 
-  return (
-    <motion.mesh position={[positionX, positionY, positionZ]} scale={scale} rotation={[rotationX1, rotationY1, 0]}>
-      <primitive object={scene} />
-    </motion.mesh>
-  );
+	let rotationX1 = useMotionValue(0);
+	let rotationY1 = useMotionValue(0);
+	let positionX = useMotionValue(0);
+	let positionY = useMotionValue(0);
+	let positionZ = useMotionValue(0);
+
+	rotationX1 = useTransform(scrollYProgress, [0, 1], ["0.3", "-1"]);
+	rotationY1 = useTransform(
+		scrollYProgress,
+		[0, 0.2, 1],
+		["-0.8", "-1.6", "-10"]
+	);
+	positionX = useTransform(
+		scrollYProgress,
+		[0, 0.2, 1],
+		[position[0], "0", "-3"]
+	);
+	positionY = useTransform(
+		scrollYProgress,
+		[0, 0.2, 1],
+		[position[1], "-2", "5"]
+	);
+	positionZ = useTransform(
+		scrollYProgress,
+		[0, 0.2, 1],
+		[position[2], "0", "0"]
+	);
+
+	return (
+		<motion.mesh
+			position={[positionX, positionY, positionZ]}
+			scale={scale}
+			rotation={[rotationX1, rotationY1, 0]}>
+			<primitive object={scene} />
+		</motion.mesh>
+	);
 };
 
 const Lights = () => {
-  const directionalLightRef = useRef()
-  const pointLightRef = useRef()
-  const spotLightRef = useRef()
-  const hemisphereLightRef = useRef()
+	const directionalLightRef = useRef();
+	const pointLightRef = useRef();
+	const spotLightRef = useRef();
+	const hemisphereLightRef = useRef();
 
-  return (
-    <>
-      <directionalLight position={[2, 2, 2]} intensity={1} ref={directionalLightRef}/>
-      <ambientLight intensity={0.3} />
-      <pointLight position={[10, 0, 10]} intensity={2} ref={pointLightRef}/>
-      <spotLight position={[-20, 0, 3]} angle={.15} penumbra={0} intensity={0.5} ref={spotLightRef}/>
-      <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} ref={hemisphereLightRef}/>
-    </>
-  )
-}
+	return (
+		<>
+			<directionalLight
+				position={[2, 2, 2]}
+				intensity={1}
+				ref={directionalLightRef}
+			/>
+			<ambientLight intensity={0.3} />
+			<pointLight position={[10, 0, 10]} intensity={2} ref={pointLightRef} />
+			<spotLight
+				position={[-20, 0, 3]}
+				angle={0.15}
+				penumbra={0}
+				intensity={0.5}
+				ref={spotLightRef}
+			/>
+			<hemisphereLight
+				skyColor='#b1e1ff'
+				groundColor='#000000'
+				intensity={1}
+				ref={hemisphereLightRef}
+			/>
+		</>
+	);
+};
 
 const VoyagerCanvas = ({ scrollContaienr }) => {
-  const [scale, setScale] = useState([2, 2, 2]);
-  const [position, setPosition] = useState([0.2, -0.7, 0]);
+	const [scale, setScale] = useState([2, 2, 2]);
+	const [position, setPosition] = useState([0.2, -0.7, 0]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 500) {
-        setScale([0.3, 0.3, 0.3]);
-        setPosition([0, 3, -5]);
-      } else if (window.innerWidth < 768) {
-        setScale([0.3, 0.3, 0.3]);
-        setPosition([1.5, 5, -5]);
-      } else if (window.innerWidth < 1024) {
-        setScale([0.33, 0.33, 0.33]);
-        setPosition([2.5, 5, -7]);
-      } else if (window.innerWidth < 1280) {
-        setScale([0.5, 0.5, 0.5]);
-        setPosition([3, 5.5, -10]);
-      } else if (window.innerWidth < 1536) {
-        setScale([0.66, 0.66, 0.66]);
-        setPosition([3, 5, -10]);
-      } else {
-        setScale([1, 1, 1]);
-        setPosition([6, 5, -15]);
-      }
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth < 500) {
+				setScale([0.3, 0.3, 0.3]);
+				setPosition([0, 3, -5]);
+			} else if (window.innerWidth < 768) {
+				setScale([0.3, 0.3, 0.3]);
+				setPosition([1.5, 5, -5]);
+			} else if (window.innerWidth < 1024) {
+				setScale([0.33, 0.33, 0.33]);
+				setPosition([2.5, 5, -7]);
+			} else if (window.innerWidth < 1280) {
+				setScale([0.5, 0.5, 0.5]);
+				setPosition([3, 5.5, -10]);
+			} else if (window.innerWidth < 1536) {
+				setScale([0.66, 0.66, 0.66]);
+				setPosition([3, 5, -10]);
+			} else {
+				setScale([1, 1, 1]);
+				setPosition([6, 5, -15]);
+			}
+		};
 
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+		handleResize();
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
-  return (
-    <Canvas className={`!absolute w-full h-screen bg-transparent z-40`} camera={{ near: 0.1, far: 1000 }}>
-      <Suspense fallback={<Loader />}>
-        <Lights />
-        <Voyager scale={scale} position={position} ref={scrollContaienr} />
-      </Suspense>
-    </Canvas>
-  );
+	return (
+		<Canvas
+			className={`!absolute w-full h-screen bg-transparent z-40`}
+			camera={{ near: 0.1, far: 1000 }}>
+			<Suspense fallback={<Loader />}>
+				<Lights />
+				<Voyager scale={scale} position={position} ref={scrollContaienr} />
+			</Suspense>
+		</Canvas>
+	);
 };
 
 export default React.memo(VoyagerCanvas);
